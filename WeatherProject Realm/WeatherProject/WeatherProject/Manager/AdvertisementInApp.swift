c46510bc523844096ba5632a40eda6a03662dcf8
Иван Селюк <>
2022-05-31 23:17:51 +0300

    add keyWindow

--- WeatherProject Realm/WeatherProject/WeatherProject/Manager/AdvertisementInApp.swift
+++ WeatherProject Realm/WeatherProject/WeatherProject/Manager/AdvertisementInApp.swift
@@ -73,10 +73,4 @@ extension UIApplication {
         }
         return controller
     }
-    
-//    let keyWindow = UIApplication.shared.connectedScenes
-//            .filter({$0.activationState == .foregroundActive})
-//            .compactMap({$0 as? UIWindowScene})
-//            .first?.windows
-//            .filter({$0.isKeyWindow}).first
 }
